f1c923f3ead46a00b4380fbd7e7ef18021198e05
Sojournerhhy <han_hongying@163.com>
2019-03-16 16:24:29 +0800

    @dev 包装返回结果

--- src/main/java/com/iridescent/apigateway/filter/ResultWrapperFilter.java
+++ src/main/java/com/iridescent/apigateway/filter/ResultWrapperFilter.java
@@ -1,9 +1,15 @@
 package com.iridescent.apigateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.netflix.ribbon.RibbonHttpResponse;
+import sun.misc.IOUtils;
 
 
-
+import java.io.IOException;
+import java.io.InputStream;
 
 
 /**
@@ -31,7 +37,31 @@ public class ResultWrapperFilter extends ZuulFilter {
 
     @Override
     public Object run() {
+        // 方法一
+        try {
+            Object zuulResponse = RequestContext.getCurrentContext().get("zuulResponse");
+            if (zuulResponse != null) {
+                RibbonHttpResponse resp = (RibbonHttpResponse) zuulResponse;
+                String body = resp.getBody().toString();
+                System.err.println(body);
+                resp.close();
+                RequestContext.getCurrentContext().setResponseBody(body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // 方法二
+        InputStream stream = RequestContext.getCurrentContext().getResponseDataStream();
+
+            String body = stream.toString();
+            System.err.println(body);
+            RequestContext.getCurrentContext().setResponseBody(body);
 
         return null;
     }
+
+
+
 }
